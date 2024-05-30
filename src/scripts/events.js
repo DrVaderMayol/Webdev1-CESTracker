662968a2eb6117ec22c0219610c388fd5d9c04d1
Erwin Antepuesto <barmosabelle@gmail.com>
2024-05-31 00:46:10 +0800

    added delete and add event button

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -31,16 +31,51 @@ async function getEvents() {
       <td class="w-2/6 py-2 px-4 border-r">${formattedDate}</td>
       <td class="w-fit py-2 px-4 border-r">${event.description}</td>
       <td class="py-2 px-4 flex justify-center space-x-2">
-        <button class="bg-green-300 x-2 py-1 rounded-xl w-24 h-12 flex flex-row justify-center items-center text-darkblue hover:bg-green-800 hover:text-white">
-          <span class="material-symbols-outlined pr-2">visibility</span>View
+
+        <button id="myBtn" class="bg-green-300 x-2 py-1 rounded-xl w-24 h-12 flex flex-row justify-center items-center text-darkblue hover:bg-green-800 hover:text-white">
+          <span class="material-symbols-outlined pr-2">visibility</span>
+          View
         </button>
+
         <button class="bg-blue-500 text-darkblue px-2 py-1 rounded-xl w-24 h-12 flex flex-row justify-center items-center hover:bg-blue-800 hover:text-white">
-          <span class="material-symbols-outlined pr-2">edit</span>Edit
+          <span class="material-symbols-outlined pr-2">edit</span>
+          Edit
         </button>
+
+        <button class="bg-red-500 text-darkblue px-2 py-1 rounded-xl w-24 h-12 flex flex-row justify-center items-center hover:bg-red-800 hover:text-white">
+        <span class="material-symbols-outlined">delete</span>
+          Delete
+        </button>
+
       </td>
     `;
     tableBody.appendChild(row);
   });
-}
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
 document.addEventListener('DOMContentLoaded', getEvents);
